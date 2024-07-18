7fd2a4964595b9035a73057fb1c833ffd0ab9ece
Kunal ahirwar <112234478+kunalahirwar2001@users.noreply.github.com>
2024-07-18 17:42:42 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+// code readeability should be high
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number = > 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value, let state = null; , typeof (null) is object 
+// undefined => let state;, typeof (undefined) is undefined
+// symbol => unique
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
