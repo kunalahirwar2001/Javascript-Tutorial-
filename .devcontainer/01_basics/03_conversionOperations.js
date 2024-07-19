2a60ac78510ddb52473c735a60d3ee37b2ee1065
Kunal ahirwar <112234478+kunalahirwar2001@users.noreply.github.com>
2024-07-19 16:26:57 +0000

    datatype Summary

--- .devcontainer/01_basics/03_conversionOperations.js
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -1,25 +1,52 @@
-let score = "33abc";
+// let score = "33abc";
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33" => 33
 //"33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = 1;
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 => true , => false
 // "" => false , "kunal" => true 
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************* Operations ***************************************
+// let value = 3;
+// let negValue = -value
+// console.log(negValue);
+
+// let str1 = "hello "
+// let str2 = "kunal"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3 + 4) * 5 % 3);
+
+console.log(true); //true
+console.log(+true); //1
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+console.log(num1);
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
---
+++ .devcontainer/05_dataTypes.js
@@ -0,0 +1,29 @@
+// primitive dataTypes, call by value, copy is created changes are made in copy 
+// 7 types : String, boolean, null, undefined, number, Symbol, BigInt
+// JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+const score = 100;
+const scoreValue = 100.3;
+const loggedIn = false;
+const outSide = null;
+let UserEmail;
+const name = "kunal";
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+
+// non Primitive dataTypes/reference Type  
+// Arrays, Objects, Functions
+const heroes = ["shaktiman", "nagraj", "doga"]
+
+let myobj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function () {
+    console.log("hello world");
+}
