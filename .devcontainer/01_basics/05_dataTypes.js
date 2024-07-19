3084cdc65123558ccbdbcc7ddd380dc869beae30
Kunal ahirwar <112234478+kunalahirwar2001@users.noreply.github.com>
2024-07-19 17:20:13 +0000

    stack and heap

--- .devcontainer/01_basics/05_dataTypes.js
+++ .devcontainer/01_basics/05_dataTypes.js
@@ -1,29 +1,51 @@
 // primitive dataTypes, call by value, copy is created changes are made in copy 
 // 7 types : String, boolean, null, undefined, number, Symbol, BigInt
 // JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
-const score = 100;
-const scoreValue = 100.3;
-const loggedIn = false;
-const outSide = null;
-let UserEmail;
-const name = "kunal";
+// const score = 100;
+// const scoreValue = 100.3;
+// const loggedIn = false;
+// const outSide = null;
+// let UserEmail;
+// const name = "kunal";
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 
 
 // non Primitive dataTypes/reference Type  
 // Arrays, Objects, Functions
-const heroes = ["shaktiman", "nagraj", "doga"]
+// const heroes = ["shaktiman", "nagraj", "doga"]
 
-let myobj = {
-    name: "hitesh",
-    age: 22,
+// let myobj = {
+//     name: "hitesh",
+//     age: 22,
+// }
+
+// const myFunction = function () {
+//     console.log("hello world");
+// }
+
+// *************************************************
+// stack (primitive)(Copy), Heap (Non - Primitive)(Reference)
+let myYoutubename = "kunalahirwardotcom"
+
+let anothername = myYoutubename
+anothername = "kunalandcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+let userOne = {
+    email:"user@google.com",
+    upi:"kunal@paytm.in"
 }
 
-const myFunction = function () {
-    console.log("hello world");
-}
+let userTwo = userOne
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
