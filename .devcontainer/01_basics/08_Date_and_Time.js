a8841b79d835523ac700cb57c2d6b2cc1ff06b09
Kunal ahirwar <112234478+kunalahirwar2001@users.noreply.github.com>
2024-07-21 17:11:58 +0000

    date

---
+++ .devcontainer/01_basics/08_Date_and_Time.js
@@ -0,0 +1,24 @@
+// Dates
+// let myDate = new Date()
+// console.log(typeof myDate);
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// let myCreatedDate = new Date(2023,0,23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+// let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+// let newTimeStamp = Date.now()
+// console.log(newTimeStamp);
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time`
+
