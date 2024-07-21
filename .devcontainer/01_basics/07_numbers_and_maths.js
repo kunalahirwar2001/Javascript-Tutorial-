de3762bb6029918272583ffcfb7bb6caeec09e39
Kunal ahirwar <112234478+kunalahirwar2001@users.noreply.github.com>
2024-07-21 16:44:59 +0000

    math and num

---
+++ .devcontainer/01_basics/07_numbers_and_maths.js
@@ -0,0 +1,33 @@
+// ****************** NUMBERS **************************
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 123.456
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ******************* MATHS ********************************
+// console.log(Math);
+// console.log(Math.abs(-4)); //4
+// console.log(Math.round(4.6)); //5
+// console.log(Math.round(4.3)); //4
+// console.log(Math.ceil(4.2)); //5
+// console.log(Math.floor(4.9)); //4
+// console.log(Math.Min(4,3,5,6,7)); //3
+// console.log(Math.Max(4,3,5,6,7)); //7
+console.log(Math.random());
+console.log(Math.random() * 10);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
